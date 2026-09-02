c172c672e377599a2ad394959068b5a8552ffaec
Yaekpp <yaekppe@gmail.com>
2026-09-02 19:21:18 +0300

    Add files via upload
    shemzone isim degisikligi

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 const searchInput = document.querySelector(".search-box input");
 const cards = document.querySelectorAll(".card");
 
-searchInput.addEventListener("input", function () {
-    const searchText = searchInput.value.toLowerCase();
+if (searchInput) {
+    searchInput.addEventListener("input", function () {
+        const searchText = searchInput.value.toLowerCase();
 
-    cards.forEach(function (card) {
-        const cardName = card.querySelector("h3").textContent.toLowerCase();
+        cards.forEach(function (card) {
+            const cardTitle = card.querySelector("h3");
 
-        if (cardName.includes(searchText)) {
-            card.style.display = "block";
-        } else {
-            card.style.display = "none";
-        }
+            if (!cardTitle) return;
+
+            const cardName = cardTitle.textContent.toLowerCase();
+
+            if (cardName.includes(searchText)) {
+                card.style.display = "block";
+            } else {
+                card.style.display = "none";
+            }
+        });
     });
-});
+}
